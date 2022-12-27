6ae5f8d9fbffc155805605e569e7130f349779ff
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-12-27 16:23:35 +0900

    feat: Add OnBoardingView

--- YNL/View/OnBoardingView.swift
+++ YNL/View/OnBoardingView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct OnBoardingView: View {
+    @AppStorage("_isFirstLaunching") var isFirstLaunching: Bool = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("OnBoarding View!")
+//            .fullScreenCover(isPresented: $isFirstLaunching) {
+//                OnBoardingView(isFirstLaunching: isFirstLaunching)
+//            }
     }
 }
 
