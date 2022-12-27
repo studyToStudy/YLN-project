1016466ad81a5ccaaee2e400316e422a47c12cc4
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-12-27 11:42:17 +0900

    Initial Commit

---
+++ YNL/YNLApp.swift
@@ -0,0 +1,20 @@
+//
+//  YNLApp.swift
+//  YNL
+//
+//  Created by 이정후 on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct YNLApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
