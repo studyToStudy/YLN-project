77e491c7832b7962b08d3ce59d8b0d08231879c7
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-12-27 16:22:49 +0900

    feat: Add MyInfoView

--- YNL/View/MyInfoView.swift
+++ YNL/View/MyInfoView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct MyInfoView: View {
+    @State var stack = NavigationPath()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $stack) {
+                    List {
+                        Section {
+//                            NavigationLink()
+                            Image(systemName: "person")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .padding(.all, 10)
+                                .clipShape(Circle())
+                            VStack(alignment: .leading, spacing: 3) {
+                                Text("이름")
+                            }
+//                            NavigationLink("Pink", value: "2")
+//                            NavigationLink("Teal", value: "3")
+//                            NavigationLink("회원탈퇴", value: "4")
+                        }
+                        .navigationDestination(for: String.self) { value in
+                            Text("Child Number \(value)")
+                            Button("뒤로가기") {
+                                stack.removeLast()
+                            }
+                        }
+                        .navigationTitle("내 정보")
+                    }
+                        }
     }
 }
 
